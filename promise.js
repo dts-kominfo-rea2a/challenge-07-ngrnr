c2bc6622caeaf74dcbc5c649f64b9997e3b764da
ngr.r <91729773+ngrnr@users.noreply.github.com>
2022-06-10 09:41:51 +0700

    Nugraha Ramadan - commited

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+    const allData = [...dataIXX, ...dataVGC];
+    const result = allData.filter((e) => e.hasil === emosi);
+    return result.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
 };
+
+
